d5747cbe796dfb8f7e3aeee2781a3b59e2e028d5
ashly <18204080028165@.cecytebc.com.mx>
2019-03-09 10:47:15 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function changetitle() {
+    var title = document.getElementById("title");
+    var firstname=document.getElementById("firstname");
+    
+    title.innerHTML="hola"+firstname.value;
+}
